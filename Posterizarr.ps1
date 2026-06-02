6ae63c4b305c35139709704814405186daf8387f
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2026-06-02 10:06:25 +0200

    Bump version.

--- Posterizarr.ps1
+++ Posterizarr.ps1
@@ -54,7 +54,7 @@ for ($i = 0; $i -lt $ExtraArgs.Count; $i++) {
     }
 }
 
-$CurrentScriptVersion = "2.2.47"
+$CurrentScriptVersion = "2.2.48"
 $global:HeaderWritten = $false
 $ProgressPreference = 'SilentlyContinue'
 
