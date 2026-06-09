f08b5aa8002908d718df7ef3584c00c36437a0c8
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2026-06-09 14:57:05 +0200

    Fix overlay variable name typo
    Correct a typo in CheckOverlayDimensions: replace '-OverlayPath $Show Backgroundoverlay' with '-OverlayPath $ShowBackgroundoverlay' so Test-Dimension receives the intended variable for the Show Background overlay and avoids argument splitting/errors when validating overlay dimensions.

--- Posterizarr.ps1
+++ Posterizarr.ps1
@@ -4794,7 +4794,7 @@ function CheckOverlayDimensions {
 
     # Standard Background/TC Types (expect BackgroundSize)
     Test-Dimension -OverlayPath $Backgroundoverlay -ExpectedSize $BackgroundSize -OverlayName "Background overlay"
-    Test-Dimension -OverlayPath $Show Backgroundoverlay -ExpectedSize $BackgroundSize -OverlayName "Show Background overlay"
+    Test-Dimension -OverlayPath $ShowBackgroundoverlay -ExpectedSize $BackgroundSize -OverlayName "Show Background overlay"
     Test-Dimension -OverlayPath $Titlecardoverlay -ExpectedSize $BackgroundSize -OverlayName "TitleCard overlay"
     Test-Dimension -OverlayPath $Backgroundoverlay4k -ExpectedSize $BackgroundSize -OverlayName "4K Background overlay"
     Test-Dimension -OverlayPath $Backgroundoverlay1080p -ExpectedSize $BackgroundSize -OverlayName "1080p Background overlay"
