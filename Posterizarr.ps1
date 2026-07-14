685ecb4902be10b28e8054c57a92584446887b08
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2026-07-14 18:20:38 +0200

    Bump version to 3.0.3
    Update script and release version numbers to 3.0.3.

--- Posterizarr.ps1
+++ Posterizarr.ps1
@@ -56,7 +56,7 @@ for ($i = 0; $i -lt $ExtraArgs.Count; $i++) {
     }
 }
 
-$CurrentScriptVersion = "3.0.2"
+$CurrentScriptVersion = "3.0.3"
 $global:HeaderWritten = $false
 $ProgressPreference = 'SilentlyContinue'
 
