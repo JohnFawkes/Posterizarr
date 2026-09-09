62fa896d3dc4939c7b9d839e38eccff71199879e
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2026-09-09 09:43:23 +0200

    Bump version to 3.3.3
    Update the app version and release metadata from 3.3.2 to 3.3.3 in both the PowerShell script and release file.

--- Posterizarr.ps1
+++ Posterizarr.ps1
@@ -75,7 +75,7 @@ for ($i = 0; $i -lt $ExtraArgs.Count; $i++) {
     }
 }
 
-$CurrentScriptVersion = "3.3.2"
+$CurrentScriptVersion = "3.3.3"
 $global:HeaderWritten = $false
 $ProgressPreference = 'SilentlyContinue'
 
